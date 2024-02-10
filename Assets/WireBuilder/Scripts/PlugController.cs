aacc0efacc720eddb0c6dca6d44b9286b0cbc854
WhatIsRove <50421316+WhatIsRove@users.noreply.github.com>
2024-02-10 14:31:41 +0000

    Added functioning cctv, updated button model

--- Assets/WireBuilder/Scripts/PlugController.cs
+++ Assets/WireBuilder/Scripts/PlugController.cs
@@ -40,7 +40,7 @@ public class PlugController : MonoBehaviour
             endAnchorRB = other.GetComponent<Rigidbody>();
             endAnchorGrabObj = other.GetComponent<Grabbable>();
 
-            endAnchorGrabObj.Drop();
+            if (endAnchorGrabObj != null) endAnchorGrabObj.Drop();
 
             endAnchorRB.isKinematic = true;
             endAnchor.position = plugPosition.position;
@@ -63,20 +63,27 @@ public class PlugController : MonoBehaviour
             Vector3 eulerRotation = new Vector3(this.transform.eulerAngles.x + 90, this.transform.eulerAngles.y, this.transform.eulerAngles.z);
             endAnchor.transform.rotation = Quaternion.Euler(eulerRotation);
 
-            if (endAnchorGrabObj.grabbed && !wasPlugged)
+            if (endAnchorGrabObj != null)
             {
-                isConected = false;
-                endAnchorRB.isKinematic = false;
-                hasPlug = false;
-                wasPlugged = true;
-                if (isOverload) GameObject.FindObjectOfType<OverloadSystem>().OverloadEverything(false);
+                if (endAnchorGrabObj.grabbed && !wasPlugged)
+                {
+                    isConected = false;
+                    endAnchorRB.isKinematic = false;
+                    hasPlug = false;
+                    wasPlugged = true;
+                    if (isOverload) GameObject.FindObjectOfType<OverloadSystem>().OverloadEverything(false);
+                }
             }
+            
         }
 
-        if (Vector3.Distance(transform.position, endAnchor.position) > replugDistance && wasPlugged)
+        if (endAnchor != null)
         {
-            if (isPuzzle) GameObject.FindObjectOfType<PlugPuzzle>().UnPlug(currentPlug);
-            wasPlugged = false;
+            if (Vector3.Distance(transform.position, endAnchor.position) > replugDistance && wasPlugged)
+            {
+                if (isPuzzle) GameObject.FindObjectOfType<PlugPuzzle>().UnPlug(currentPlug);
+                wasPlugged = false;
+            }
         }
     }
 
