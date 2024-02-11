ae015a3dda4a363028ee52829ddb1ee834e06339
WhatIsRove <50421316+WhatIsRove@users.noreply.github.com>
2024-02-11 23:39:26 +0000

    Tweaked lighting, added clipboard

--- Assets/Scripts/Grabbable.cs
+++ Assets/Scripts/Grabbable.cs
@@ -11,6 +11,8 @@ public class Grabbable : MonoBehaviour
 
     public bool grabbed = false;
 
+    public bool facePlayer = false;
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
@@ -22,6 +24,12 @@ public class Grabbable : MonoBehaviour
         {
             Vector3 targetPos = grabPoint.position - transform.position;
             rb.velocity = new Vector3(targetPos.x, targetPos.y, targetPos.z) * grabSpeed;
+
+            if (facePlayer)
+            {
+                var targetRotation = Quaternion.LookRotation(Camera.main.transform.position - transform.position, transform.up);
+                rb.rotation = targetRotation;
+            }
         }
     }
 
