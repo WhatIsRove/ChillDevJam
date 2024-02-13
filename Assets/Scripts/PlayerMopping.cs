7623fa558b2a46deecfc6b24beba84d75105b650
BrendanMayer <brendan.mayer1234@gmail.com>
2024-02-13 00:48:10 +0000

    New Models And Mopping

--- Assets/Scripts/PlayerMopping.cs
+++ Assets/Scripts/PlayerMopping.cs
@@ -6,55 +6,9 @@ public class PlayerMopping : MonoBehaviour
 {
 
     public LayerMask moppingLayer;
-    public LayerMask lightSwitchLayer;
-    private bool isMopping = false;
-    private bool isCoroutineRunning = false;
-    private Camera camera;
+   
 
-    void Start()
-    {
-        camera = Camera.main;
-    }
-
-    void Update()
-    {
-        if (Input.GetMouseButtonDown(0)) // Assuming left mouse button
-        {
-            isMopping = true;
-            CheckLightSwitch();
-            StartCoroutine(MopRepeatedly());
-        }
-        else if (Input.GetMouseButtonUp(0))
-        {
-            isMopping = false;
-            
-            StopCoroutine(MopRepeatedly());
-        }
-    }
-
-    IEnumerator MopRepeatedly()
-    {
-        
-        if (isMopping)
-        {
-            if (!isCoroutineRunning)
-            {
-                isCoroutineRunning = true; // Set the flag to indicate that the coroutine is running
-                RaycastHit hit;
-                if (Physics.Raycast(camera.transform.position, camera.transform.forward, out hit, 50, moppingLayer))
-                {
-                    Puddle puddle = hit.transform.gameObject.GetComponent<Puddle>();
-                    if (puddle != null)
-                    {
-                        Mop(puddle);
-                    }
-                }
-                yield return new WaitForSeconds(1f); // Wait for 1 second before repeating
-            }
-            
-        }
-        isCoroutineRunning = false; // Reset the flag when the coroutine finishes
-    }
+  
 
     void Mop(Puddle puddle)
     {
@@ -63,15 +17,27 @@ public class PlayerMopping : MonoBehaviour
         puddle.ReducePuddle();
     }
 
-    private void CheckLightSwitch()
+
+    void OnTriggerEnter(Collider other)
     {
-        RaycastHit hit;
-        if (Physics.Raycast(camera.transform.position, camera.transform.forward, out hit, 50, lightSwitchLayer))
+        
+           
+        if (other.gameObject.layer == LayerMask.NameToLayer("Puddle"))
         {
-            
-            LightSwitch.Instance.FlipSwitch();
+            Puddle puddle = other.gameObject.GetComponent<Puddle>();
+            if (puddle != null)
+            {
+                Mop(puddle);
+            }
         }
+ 
     }
+
+    void OnTriggerExit(Collider other)
+    {
+        
+    }
+
 
 
 
